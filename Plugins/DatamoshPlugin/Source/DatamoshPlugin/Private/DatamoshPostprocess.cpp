f84a1b1021e7a2b906ff582050f2d06c6d9d7d78
Torphedo <73564623+Torphedo@users.noreply.github.com>
2025-03-16 11:26:13 -0400

    Minor cleanups

--- Plugins/DatamoshPlugin/Source/DatamoshPlugin/Private/DatamoshPostprocess.cpp
+++ Plugins/DatamoshPlugin/Source/DatamoshPlugin/Private/DatamoshPostprocess.cpp
@@ -113,19 +113,16 @@ FScreenPassTexture FCustomSceneViewExtension::CustomPostProcessing(FRDGBuilder& 
 		FRDGTextureRef outputTexture = GraphBuilder.CreateTexture(OutputDesc, TEXT("Datamosh Output Framebuffer"), ERDGTextureFlags::None);
 		FRDGTextureRef history = GraphBuilder.CreateTexture(OutputDesc, TEXT("Datamosh Historical Framebuffer"), ERDGTextureFlags::MultiFrame);
 
-		static bool frozen_last_frame = false;
 		if (CVarFreezeFrame.GetValueOnRenderThread()) {
-			frozen_last_frame = true;
 			if (historyBuffer != nullptr) {
                 history = GraphBuilder.RegisterExternalTexture(historyBuffer);
 			}
-		} else if (frozen_last_frame) {
-			frozen_last_frame = false;
-            AddCopyTexturePass(GraphBuilder, SceneColor.Texture, history);
+			PassParameters->OriginalSceneColor = history;
+		} else {
+			PassParameters->OriginalSceneColor = SceneColor.Texture;
 		}
 		
 		// Create UAV from target texture
-        PassParameters->OriginalSceneColor = SceneColor.Texture;
 		PassParameters->Output = GraphBuilder.CreateUAV(outputTexture);
         PassParameters->historyBuffer = GraphBuilder.CreateUAV(history);
 		
